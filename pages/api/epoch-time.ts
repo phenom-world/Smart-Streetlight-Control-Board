3217c5f4ccc719897ce7be2f9df37fd69ad7cb31
phenom-world <tijjken@gmail.com>
2024-03-06 01:18:34 +0100

    chore: minor update

--- pages/api/epoch-time.ts
+++ pages/api/epoch-time.ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'OPTION') {
-    res.status(200).json({ message: 'OK' });
-  }
   if (req.method === 'POST') {
     res.status(200).json({ time: req.body.epochTime });
   }
