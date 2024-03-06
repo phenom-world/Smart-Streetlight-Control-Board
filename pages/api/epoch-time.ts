b3d61d686be62c9bc71541f8ddc64eecd8cbcd63
phenom-world <tijjken@gmail.com>
2024-03-06 07:35:44 +0100

    chore: minor update

--- pages/api/epoch-time.ts
+++ pages/api/epoch-time.ts
@@ -3,14 +3,17 @@ import { promises as fs } from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const epochTime = req.body.epochTime;
   const jsonData = await fs.readFile(filePath, 'utf8');
-  const objectData = JSON.parse(jsonData.length === 0 ? '{}' : jsonData);
-  objectData.epochTime = epochTime;
-  const updatedData = JSON.stringify(objectData);
-  await fs.writeFile(filePath, updatedData);
 
   if (req.method === 'POST') {
+      const epochTime = req.body.epochTime;
+    const objectData = JSON.parse(jsonData.length === 0 ? '{}' : jsonData);
+    objectData.epochTime = epochTime;
+    const updatedData = JSON.stringify(objectData);
+    await fs.writeFile(filePath, updatedData);
     res.status(200).json({ data: updatedData });
+  }
+  if (req.method === 'GET') {
+    res.status(200).json({ data: jsonData });
   }
 }
